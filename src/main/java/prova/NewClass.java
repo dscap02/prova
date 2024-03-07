7b54a77dcf369fd5f60564d31a086dc24fdca92d
dscap02 <d.scaparra@studenti.unisa.it>
2024-03-07 15:22:18 +0100

    added comment to main class

--- src/main/java/prova/NewClass.java
+++ src/main/java/prova/NewClass.java
@@ -4,7 +4,8 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Change 2");
+		//Branchiamo
+		System.out.println("New changes");
 
 	}
 
