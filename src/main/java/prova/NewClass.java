c003d7365c03927fd06cd22a2c6e9fe18ea89382
dscap02 <d.scaparra@studenti.unisa.it>
2024-03-07 15:31:15 +0100

    make a message shorter

--- src/main/java/prova/NewClass.java
+++ src/main/java/prova/NewClass.java
@@ -5,7 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Branchiamo
-		System.out.println("New changes");
+		System.out.println("Ne");
 
 	}
 
